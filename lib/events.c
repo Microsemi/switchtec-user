d868b34225b09c5d6026d8ca9b28cff750011cf0
Logan Gunthorpe <logang@deltatee.com>
2017-11-01 11:41:40 -0600

    Fix sparse warning for mixing enum types
    lib/events.c:251:25: error: mixing different enum types
    lib/events.c:251:25:     int enum <noident>  versus
    lib/events.c:251:25:     int enum switchtec_event_type

--- lib/events.c
+++ lib/events.c
@@ -45,13 +45,13 @@
 	.desc = d, \
 }
 
+#define GLOBAL SWITCHTEC_EVT_GLOBAL
+#define PART SWITCHTEC_EVT_PART
+#define PFF SWITCHTEC_EVT_PFF
+
 static const struct {
 	enum switchtec_event_id id;
-	enum {
-		GLOBAL = SWITCHTEC_EVT_GLOBAL,
-		PART = SWITCHTEC_EVT_PART,
-		PFF = SWITCHTEC_EVT_PFF,
-	} type;
+	enum switchtec_event_type type;
 	uint64_t summary_bit;
 	int ioctl_id;
 	const char *short_name;
