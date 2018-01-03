924496203e0a20cdee6fd3653e7a8303b8117c4f
Logan Gunthorpe <logang@deltatee.com>
2018-01-02 17:18:32 -0700

    Fix bug with GLOBAL events not being able to be selected in summary structs
    This made it impossible to wait for global events seeing it would always wait
    on an empty wait_for structure.

--- lib/events.c
+++ lib/events.c
@@ -179,7 +179,7 @@ int switchtec_event_summary_set(struct switchtec_event_summary *sum,
 
 	switch (events[e].type) {
 	case GLOBAL:
-		sum->global &= bit;
+		sum->global |= bit;
 		break;
 	case PART:
 		if (index == SWITCHTEC_EVT_IDX_LOCAL) {
