Microsemi/switchtec-user

commit 50a6136121e518dc51a1347f052587e21455e374

Add new option --bw_type for command gui and bw. User can get raw bandwidth or payload bandwidth with this option.

cli/gui.c
@@ -327,7 +327,8 @@ free_and_return:
 }
 
 static int gui_init(struct switchtec_dev *dev, unsigned reset,
-		    struct switchtec_bwcntr_res *bw_data)
+		    struct switchtec_bwcntr_res *bw_data,
+		    enum switchtec_bw_type bw_type)
 {
 	int ret, p, numports, port_ids[SWITCHTEC_MAX_PORTS];
 	struct switchtec_status *status;
@@ -344,6 +345,10 @@ retry:
 
 	for (p = 0; p < numports; p++)
 		port_ids[p] = status[p].port.phys_id;
+
+	ret = switchtec_bwcntr_set_many(dev, numports, port_ids, bw_type);
+	if (ret < 0)
+		cleanup_and_error("Set bandwidth type");
 
 	if (reset)
 		ret = switchtec_bwcntr_many(dev, numports, port_ids, 1, NULL);
@@ -387,7 +392,7 @@ static unsigned gui_keypress(void)
   /* Main GUI window. */
 
 int gui_main(struct switchtec_dev *dev, unsigned all_ports, unsigned reset,
-	     unsigned refresh, int duration)
+	     unsigned refresh, int duration, enum switchtec_bw_type bw_type)
 {
 	struct timeval endtime, now;
 
@@ -405,7 +410,7 @@ int gui_main(struct switchtec_dev *dev, unsigned all_ports, unsigned reset,
 	int ret;
 	struct switchtec_bwcntr_res bw_data[SWITCHTEC_MAX_PORTS] = { {0} };
 
-	ret = gui_init(dev, reset, bw_data);
+	ret = gui_init(dev, reset, bw_data, bw_type);
 	if (ret < 0) {
 		cleanup_and_error("gui_init");
 		return ret;

cli/gui.h
@@ -27,6 +27,7 @@
 
 #include <switchtec/switchtec.h>
 
-int gui_main(struct switchtec_dev *, unsigned, unsigned, unsigned, int);
+int gui_main(struct switchtec_dev *, unsigned, unsigned, unsigned, int,
+	     enum switchtec_bw_type);
 
 #endif

cli/main.c
@@ -42,6 +42,12 @@
 
 static struct switchtec_dev *global_dev = NULL;
 
+static const struct argconfig_choice bandwidth_types[] = {
+	{"RAW", SWITCHTEC_BW_TYPE_RAW, "Get the raw bandwidth"},
+	{"PAYLOAD", SWITCHTEC_BW_TYPE_PAYLOAD, "Get the payload bandwidth"},
+	{}
+};
+
 int switchtec_handler(const char *optarg, void *value_addr,
 		      const struct argconfig_options *opt)
 {
@@ -133,9 +139,11 @@ static int gui(int argc, char **argv)
 		unsigned reset_bytes;
 		unsigned refresh;
 		int duration;
+		enum switchtec_bw_type bw_type;
 	} cfg = {
 	    .refresh  = 1,
 	    .duration = -1,
+	    .bw_type  = SWITCHTEC_BW_TYPE_RAW,
 	};
 
 	const struct argconfig_options opts[] = {
@@ -148,12 +156,14 @@ static int gui(int argc, char **argv)
 		 "gui refresh period in seconds (default: 1 second)"},
 		{"duration", 'd', "", CFG_INT, &cfg.duration, required_argument,
 		 "gui duration in seconds (-1 forever)"},
+		{"bw_type", 'b', "TYPE", CFG_CHOICES, &cfg.bw_type,
+		 required_argument, "gui bandwidth type", .choices=bandwidth_types},
 		{NULL}};
 
 	argconfig_parse(argc, argv, desc, opts, &cfg, sizeof(cfg));
 
 	ret = gui_main(cfg.dev, cfg.all_ports, cfg.reset_bytes, cfg.refresh,
-		       cfg. duration);
+		       cfg.duration, cfg.bw_type);
 
 	return ret;
 }
@@ -275,9 +285,12 @@ static int bw(int argc, char **argv)
 		struct switchtec_dev *dev;
 		unsigned meas_time;
 		int verbose;
+		enum switchtec_bw_type bw_type;
 	} cfg = {
 		.meas_time = 5,
+		.bw_type = SWITCHTEC_BW_TYPE_RAW,
 	};
+
 	const struct argconfig_options opts[] = {
 		DEVICE_OPTION,
 		{"time", 't', "NUM", CFG_POSITIVE, &cfg.meas_time,
@@ -285,9 +298,17 @@ static int bw(int argc, char **argv)
 		 "measurement time, in seconds"},
 		{"verbose", 'v', "", CFG_NONE, &cfg.verbose, no_argument,
 		 "print posted, non-posted and completion results"},
+		{"bw_type", 'b', "TYPE", CFG_CHOICES, &cfg.bw_type,
+		 required_argument, "bandwidth type", .choices=bandwidth_types},
 		{NULL}};
 
 	argconfig_parse(argc, argv, desc, opts, &cfg, sizeof(cfg));
+
+	ret = switchtec_bwcntr_set_all(cfg.dev, cfg.bw_type);
+	if (ret < 0) {
+		switchtec_perror("bw type");
+		return ret;
+	}
 
 	ret = switchtec_bwcntr_all(cfg.dev, 0, &port_ids, &before);
 	if (ret < 0) {

commit 7b1643abe9f912f283f688dffa4b655baaeb08e9

Fix build regression in recent gui_main() change
There are two gui_main() functions. When libcurses is not available
(ie. when cross compling for windows), a stub function is used.
A recent change to the gui_main() prototype forgot to make the
analagous change to the stub function and therefore caused
a build regression.

While we are at it fix, the function prototype in gui.h which should have
proper argument names.

This was caught by travis on the latest devel branch.

Signed-off-by: Logan Gunthorpe <logang@deltatee.com>
Fixes: 50a6136121e5 ("Add new option --bw_type for command gui and...")
Signed-off-by: Logan Gunthorpe <logang@deltatee.com>

cli/gui.c
@@ -447,7 +447,7 @@ int gui_main(struct switchtec_dev *dev, unsigned all_ports, unsigned reset,
 #else
 
 int gui_main(struct switchtec_dev *dev, unsigned all_ports, unsigned reset,
-	     unsigned refresh, int duration)
+	     unsigned refresh, int duration, enum switchtec_bw_type bw_type)
 {
 	printf("gui requires libcurses support when switchtec-user is built\n");
 	return 0;

cli/gui.h
@@ -27,7 +27,7 @@
 
 #include <switchtec/switchtec.h>
 
-int gui_main(struct switchtec_dev *, unsigned, unsigned, unsigned, int,
-	     enum switchtec_bw_type);
+int gui_main(struct switchtec_dev *dev, unsigned all_ports, unsigned reset,
+	     unsigned refresh, int duration, enum switchtec_bw_type bw_type);
 
 #endif