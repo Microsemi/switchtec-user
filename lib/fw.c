ff807d3cc8caa893a350ecf9200123b1a28f6372
Logan Gunthorpe <logang@deltatee.com>
2019-09-03 16:45:47 -0600

    Merge switchtec_fw_read_footer_gen3() into switchtec_fw_info_metadata()

--- lib/fw.c
+++ lib/fw.c
@@ -491,64 +491,30 @@ static int switchtec_fw_map_get_active(struct switchtec_dev *dev,
 	return 0;
 }
 
-/**
- * @brief Read a Switchtec device's firmware partition footer
- * @param[in]  dev		Switchtec device handle
- * @param[in]  partition_start	Partition start address
- * @param[in]  partition_len	Partition length
- * @param[out] ftr		The footer structure to populate
- * @param[out] version		Optional pointer to a string which will
- *	be populated with a human readable string of the version
- * @param[in]  version_len	Maximum length of the version string
- * @return 0 on success, error code on failure
- */
-static int switchtec_fw_read_footer_gen3(struct switchtec_dev *dev,
-					 unsigned long partition_start,
-					 size_t partition_len,
-					 struct switchtec_fw_footer_gen3 *ftr,
-					 char *version, size_t version_len)
-{
-	int ret;
-	unsigned long addr = partition_start + partition_len - sizeof(*ftr);
-
-	if (!ftr)
-		return -EINVAL;
-
-	ret = switchtec_fw_read(dev, addr, sizeof(*ftr), ftr);
-	if (ret < 0)
-		return ret;
-
-	if (strcmp(ftr->magic, "PMC") != 0) {
-		errno = ENOEXEC;
-		return -errno;
-	}
-
-	if (version)
-		version_to_string(ftr->version, version, version_len);
-
-	return 0;
-}
-
 static int switchtec_fw_info_metadata(struct switchtec_dev *dev,
 				      struct switchtec_fw_image_info *inf)
 {
 	struct switchtec_fw_footer_gen3 *metadata;
+	unsigned long addr;
 	int ret;
 
 	metadata = malloc(sizeof(*metadata));
 	if (!metadata)
 		return -1;
 
-	ret = switchtec_fw_read_footer_gen3(dev, inf->part_addr,
-					    inf->part_len, metadata,
-					    inf->version,
-					    sizeof(inf->version));
-	if (ret < 0) {
+	addr = inf->part_addr + inf->part_len - sizeof(*metadata);
+
+	ret = switchtec_fw_read(dev, addr, sizeof(*metadata), metadata);
+
+	if (ret < 0 ||
+	    strncmp(metadata->magic, "PMC", sizeof(metadata->magic))) {
 		inf->version[0] = 0;
 		inf->image_crc = 0xFFFFFFFF;
 		inf->metadata = NULL;
 		free(metadata);
 	} else {
+		version_to_string(metadata->version, inf->version,
+				  sizeof(inf->version));
 		inf->image_crc = metadata->image_crc;
 		inf->image_addr = metadata->load_addr;
 		inf->image_len = metadata->image_len;
