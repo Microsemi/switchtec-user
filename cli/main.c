37abea81c5db7ef8553f7755915425ce1c8b173a
Kelvin Cao <kelvin.cao@microchip.com>
2019-12-13 10:40:23 +0800

    Fix fw_toggle() error info on failure
    In fw_toggle(), after the toggle operation, it will get fw info and
    display. In the case of a failed toggle operation, it is observed that
    the fw-toggle command still show suceess after the fw info display.
    
    The reason is that the errno might be updated between the toggle
    function and switchtec_perror(), so the errno processed may not the
    one from toggle function.
    
    As we do want to show the success or failure info at the of the output,
    therefore we use a temporary var to hold it after toggle and pass it to
    switchtec_perror() at the end.

--- cli/main.c
+++ cli/main.c
@@ -1390,6 +1390,7 @@ static int fw_toggle(int argc, char **argv)
 {
 	const char *desc = "Toggle active and inactive firmware partitions";
 	int ret = 0;
+	int err = 0;
 
 	static struct {
 		struct switchtec_dev *dev;
@@ -1425,11 +1426,16 @@ static int fw_toggle(int argc, char **argv)
 							   cfg.key,
 							   cfg.firmware,
 							   cfg.config);
+		err = errno;
 	}
 
-	print_fw_part_info(cfg.dev);
+	ret = print_fw_part_info(cfg.dev);
+	if (ret)
+		switchtec_perror("print fw info");
+
 	printf("\n");
 
+	errno = err;
 	switchtec_perror("firmware toggle");
 
 	return ret;
