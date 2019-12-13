43a296adffea09eea34d878a8e5ab534a78c1f2b
xyan264 <Paul.Yan@microchip.com>
2019-12-13 13:55:36 -0800

    Add 'info' command to 'mfg' submenu.
    In BL1/Main boot phase this command shows full information, while in BL2 it only shows boot phase and secure version information.

--- cli/mfg.c
+++ cli/mfg.c
@@ -32,6 +32,7 @@
 
 #include <switchtec/switchtec.h>
 #include <switchtec/utils.h>
+#include <switchtec/mfg.h>
 #include <switchtec/endian.h>
 
 #include <locale.h>
@@ -42,6 +43,20 @@
 #include <errno.h>
 #include <stdio.h>
 #include <string.h>
+
+static const char* phase_id_to_string(enum switchtec_boot_phase phase_id)
+{
+	switch(phase_id) {
+	case SWITCHTEC_BOOT_PHASE_BL1:
+		return "BL1";
+	case SWITCHTEC_BOOT_PHASE_BL2:
+		return "BL2";
+	case SWITCHTEC_BOOT_PHASE_FW:
+		return "Main Firmware";
+	default:
+		return "Unknown Phase";
+	}
+}
 
 static int ping(int argc, char **argv)
 {
@@ -65,27 +80,158 @@ static int ping(int argc, char **argv)
 	}
 
 	printf("Mfg Ping: \t\tSUCCESS\n");
-	printf("Current Boot Phase: \t");
-	switch(phase_id) {
-	case SWITCHTEC_BOOT_PHASE_BL1:
-		printf("BL1\n");
+	printf("Current Boot Phase: \t%s\n", phase_id_to_string(phase_id));
+
+	return 0;
+}
+
+static void print_security_config(struct switchtec_security_cfg_stat *state)
+{
+	int key_idx;
+	int i;
+	static char *spi_rate_str[] = {
+		"100", "67", "50", "40", "33.33", "28.57",
+		"25", "22.22", "20", "18.18"
+	};
+
+	printf("\nBasic Secure Settings %s\n",
+		state->basic_setting_valid? "(Valid)":"(Invalid)");
+
+	printf("\tSecure State: \t\t\t");
+	switch(state->secure_state) {
+	case SWITCHTEC_UNINITIALIZED_UNSECURED:
+		printf("UNINITIALIZED_UNSECURED\n");
 		break;
-	case SWITCHTEC_BOOT_PHASE_BL2:
-		printf("BL2\n");
+	case SWITCHTEC_INITIALIZED_UNSECURED:
+		printf("INITIALIZED_UNSECURED\n");
 		break;
-	case SWITCHTEC_BOOT_PHASE_FW:
-		printf("Main Firmware\n");
+	case SWITCHTEC_INITIALIZED_SECURED:
+		printf("INITIALIZED_SECURED\n");
 		break;
 	default:
-		printf("Unknown Phase\n");
-		return -2;
+		printf("Unsupported State\n");
+		break;
 	}
+
+	printf("\tJTAG/EJTAG Debug State: \t");
+	switch(state->debug_mode) {
+	case SWITCHTEC_DEBUG_MODE_ENABLED:
+		printf("Always Enabled\n");
+		break;
+	case SWITCHTEC_DEBUG_MODE_DISABLED_BUT_ENABLE_ALLOWED:
+		printf("Disabled by Default But Can Be Enabled\n");
+		break;
+	case SWITCHTEC_DEBUG_MODE_DISABLED:
+		printf("Always Disabled\n");
+		break;
+	default:
+		printf("Unsupported State\n");
+		break;
+	}
+
+	printf("\tJTAG/EJTAG State After Reset: \t%d\n",
+		state->jtag_lock_after_reset);
+
+	printf("\tJTAG/EJTAG State After BL1: \t%d\n",
+		state->jtag_lock_after_bl1);
+
+	printf("\tJTAG/EJTAG Unlock IN BL1: \t%d\n",
+		state->jtag_bl1_unlock_allowed);
+
+	printf("\tJTAG/EJTAG Unlock AFTER BL1: \t%d\n",
+		state->jtag_post_bl1_unlock_allowed);
+
+	printf("\tSPI Clock Rate: \t\t%s MHz\n",
+		spi_rate_str[state->spi_clk_rate-1]);
+
+	printf("\tI2C Recovery TMO: \t\t%d Second(s)\n",
+		state->i2c_recovery_tmo);
+	printf("\tI2C Port: \t\t\t%d\n", state->i2c_port);
+	printf("\tI2C Address (7-bits): \t\t0x%02x\n", state->i2c_addr);
+	printf("\tI2C Command Map: \t\t0x%08x\n\n", state->i2c_cmd_map);
+
+	printf("Exponent Hex Data %s: \t\t0x%08x\n",
+		state->public_key_exp_valid? "(Valid)":"(Invalid)",
+		state->public_key_exponent);
+
+	printf("KMSK Entry Number %s: \t\t%d\n",
+		state->public_key_num_valid? "(Valid)":"(Invalid)",
+		state->public_key_num);
+
+	if (state->public_key_ver)
+		printf("Current KMSK index %s: \t\t%d\n",
+			state->public_key_ver_valid? "(Valid)":"(Invalid)",
+			state->public_key_ver);
+	else
+		printf("Current KMSK index %s: \t\tNot Set\n",
+			state->public_key_ver_valid? "(Valid)":"(Invalid)");
+
+	for(key_idx = 0; key_idx < state->public_key_num; key_idx++) {
+		printf("KMSK Entry %d:  ", key_idx + 1);
+		for(i = 0; i < SWITCHTEC_KMSK_LEN; i++)
+				printf("%02x", state->public_key[key_idx][i]);
+		printf("\n");
+	}
+}
+
+static int info(int argc, char **argv)
+{
+	const char *desc = "Display security settings (BL1 and Main Firmware only)";
+	int ret;
+	enum switchtec_boot_phase phase_id;
+
+	struct switchtec_sn_ver_info sn_info = {};
+
+	static struct {
+		struct switchtec_dev *dev;
+	} cfg = {};
+
+	const struct argconfig_options opts[] = {
+		DEVICE_OPTION_NO_PAX,
+		{NULL}
+	};
+
+	struct switchtec_security_cfg_stat state = {};
+
+	argconfig_parse(argc, argv, desc, opts, &cfg, sizeof(cfg));
+
+	ret = switchtec_get_device_info(cfg.dev, &phase_id, NULL, NULL);
+	if (ret < 0) {
+		switchtec_perror("mfg info");
+		return ret;
+	}
+	printf("Current Boot Phase: \t\t\t%s\n", phase_id_to_string(phase_id));
+
+	ret = switchtec_sn_ver_get(cfg.dev, &sn_info);
+	if (ret < 0) {
+		switchtec_perror("mfg info");
+		return ret;
+	}
+	printf("Chip Serial: \t\t\t\t0x%08x\n", sn_info.chip_serial);
+	printf("Key Manifest Secure Version: \t\t0x%08x\n", sn_info.ver_km);
+	printf("BL2 Secure Version: \t\t\t0x%08x\n", sn_info.ver_bl2);
+	printf("Main Secure Version: \t\t\t0x%08x\n", sn_info.ver_main);
+	printf("Secure Unlock Version: \t\t\t0x%08x\n", sn_info.ver_sec_unlock);
+
+	if (phase_id == SWITCHTEC_BOOT_PHASE_BL2) {
+		printf("\nOther secure settings are only shown in BL1 or Main Firmware phase.\n\n");
+		return 0;
+	}
+
+	ret = switchtec_security_config_get(cfg.dev, &state);
+	if (ret < 0) {
+		switchtec_perror("mfg info");
+		return ret;
+	}
+
+	print_security_config(&state);
 
 	return 0;
 }
 
 static const struct cmd commands[] = {
 	{"ping", ping, "Ping firmware and get current boot phase"},
+	{"info", info, "Display security settings"},
 };
 
 static struct subcommand subcmd = {
