24afd9663dad6daa4fb214356da782777662591e
Logan Gunthorpe <logang@deltatee.com>
2021-04-16 15:23:33 -0600

    cli/diag: Introduce diag sub command rcvr_obj
    The recv_obj sub command uses the library function to print the
    coefficients for each lane in a selected port.

---
+++ cli/diag.c
@@ -0,0 +1,142 @@
+/*
+ * Microsemi Switchtec(tm) PCIe Management Command Line Interface
+ * Copyright (c) 2021, Microsemi Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+#include "commands.h"
+#include "argconfig.h"
+#include "common.h"
+
+#include <switchtec/switchtec.h>
+#include <switchtec/utils.h>
+
+#include <stdio.h>
+
+struct diag_common_cfg {
+	struct switchtec_dev *dev;
+	struct switchtec_status port;
+	int port_id;
+};
+
+#define DEFAULT_DIAG_COMMON_CFG {	\
+	.port_id = -1,			\
+}
+
+#define PORT_OPTION {							\
+	"port", 'p', "PORT_ID", CFG_POSITIVE, &cfg.port_id,		\
+	required_argument, "physical port ID to dump data for",		\
+}
+
+static int get_port(struct switchtec_dev *dev, int port_id,
+		    struct switchtec_status *port)
+{
+	struct switchtec_status *status;
+	int i, ports;
+
+	ports = switchtec_status(dev, &status);
+	if (ports < 0) {
+		switchtec_perror("status");
+		return ports;
+	}
+
+	for (i = 0; i < ports; i++) {
+		if (status[i].port.phys_id == port_id ||
+		    (port_id == -1 && status[i].port.upstream)) {
+			*port = status[i];
+			switchtec_status_free(status, ports);
+			return 0;
+		}
+	}
+
+	fprintf(stderr, "Invalid physical port id: %d\n", port_id);
+	switchtec_status_free(status, ports);
+	return -1;
+}
+
+static int diag_parse_common_cfg(int argc, char **argv, const char *desc,
+				 struct diag_common_cfg *cfg,
+				 const struct argconfig_options *opts)
+{
+	int ret;
+
+	argconfig_parse(argc, argv, desc, opts, cfg, sizeof(*cfg));
+
+	ret = get_port(cfg->dev, cfg->port_id, &cfg->port);
+	if (ret)
+		return ret;
+
+	cfg->port_id = cfg->port.port.phys_id;
+
+	return 0;
+}
+
+#define CMD_DESC_RCVR_OBJ "Dump analog RX coefficients/adaptation objects"
+
+static int rcvr_obj(int argc, char **argv)
+{
+	struct diag_common_cfg cfg = DEFAULT_DIAG_COMMON_CFG;
+	struct switchtec_rcvr_obj obj;
+	int i, j, ret;
+
+	const struct argconfig_options opts[] = {
+		DEVICE_OPTION, PORT_OPTION, {}
+	};
+
+	ret = diag_parse_common_cfg(argc, argv, CMD_DESC_RCVR_OBJ,
+				    &cfg, opts);
+	if (ret)
+		return ret;
+
+	printf("Coefficients for physical port %d\n\n", cfg.port_id);
+	printf("Lane  CTLE  Tgt-Amp  Spec-DFE  DFE0 DFE1 DFE2 DFE3 DFE4 DFE5 DFE6\n");
+
+	for (i = 0; i < cfg.port.neg_lnk_width; i++) {
+		ret = switchtec_diag_rcvr_obj(cfg.dev, cfg.port_id, i,
+					      &obj);
+		if (ret) {
+			switchtec_perror("rcvr_obj");
+			return -1;
+		}
+
+		printf("%4d  %4d  %6d   %7d   ", i, obj.ctle,
+		       obj.target_amplitude, obj.speculative_dfe);
+		for (j = 0; j < ARRAY_SIZE(obj.dynamic_dfe); j++)
+			printf("%4d ", obj.dynamic_dfe[j]);
+		printf("\n");
+	}
+
+	return 0;
+}
+
+static const struct cmd commands[] = {
+	CMD(rcvr_obj,		CMD_DESC_RCVR_OBJ),
+	{}
+};
+
+static struct subcommand subcmd = {
+	.name = "diag",
+	.cmds = commands,
+	.desc = "Diagnostic Information",
+	.long_desc = "These functions provide diagnostic information from "
+		"the switch",
+};
+
+REGISTER_SUBCMD(subcmd);
