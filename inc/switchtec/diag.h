f4fe1dbacd24bbd9f91bead5722b0882c8fb1a71
BenReed161 <benreed447@gmail.com>
2025-04-15 15:28:33 -0700

    Added tlp injection command
    TLP injection command added to use the defined MRPC interface
    to send a raw TLP to a given destination port. User defined type
    and enable the ecrc.
    
    Raw tlp data is accepted as a string of characters and converted to
    appropriate dword type and sent as part of new structure defined by
    spec. Up to 132 dwords of raw tlp data can be sent a time, this limit is
    defined by a macro in the switchtec header file.

--- inc/switchtec/diag.h
+++ inc/switchtec/diag.h
@@ -25,6 +25,8 @@
 #ifndef LIBSWITCHTEC_DIAG_H
 #define LIBSWITCHTEC_DIAG_H
 
+#include "switchtec.h"
+
 #include <stdint.h>
 
 /**
@@ -277,6 +279,14 @@ struct switchtec_diag_ltssm_log_dmp_out {
 	uint32_t ram_timestamp;
 	uint32_t unused;
 	uint32_t arc;
+};
+
+struct switchtec_tlp_inject_in {
+	uint32_t dest_port;
+	uint32_t tlp_type;
+	uint32_t tlp_length;
+	uint32_t ecrc;
+	uint32_t raw_tlp_data[SWITCHTEC_DIAG_MAX_TLP_DWORDS];
 };
 
 #endif
